ae5e061f77dc729c5108ade13d0b3dd1c66c9a56
AiltonSeverino <82883207+AiltonSeverino@users.noreply.github.com>
2022-11-11 18:38:05 -0300

    Add files via upload

---
+++ Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemadeTarefas.Data.Map;
+using SistemadeTarefas.Models;
+
+namespace SistemadeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.ApplyConfiguration(new UsuarioMap());
+            modelbuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelbuilder);
+        }
+    }
+}
